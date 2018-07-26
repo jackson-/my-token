2edcdb8b731fcc699ebfe1745bf8ae8f8af7c4d0
jackson- <jackson.t.devin@gmail.com>
2018-07-26 12:03:54 -0400

    Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
-    // Constructor
+    string public name = "DappToken";
+    
+    string public symbol = "DAPP";
+
+    string public standard = "DappToken 1.0";
+
     uint256 public totalSupply;
 
-    constructor(uint256 _totalSupply) public {
-        totalSupply = _totalSupply;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
-    // Set the total number of tokens
-    // Read the total number of tokens
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,11 +1,56 @@
 var DappToken = artifacts.require("./DappToken.sol");
 contract('DappToken', function(accounts){
-    it('sets the total upon deployment', function() {
+    var tokenInstance;
+    
+    it('initializes the contract with the correct values', function() {
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, "DappToken")
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, "DAPP")
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, "DappToken 1.0")
+        })
+    })
+    
+    it('allocated the initial supply upon deployment', function() {
         return DappToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(), 1000000, 'sets the total number to 1 Mil')
+            return tokenInstance.balanceOf(accounts[0])
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(), 1000000, 'it allocates initial supply to admin account')
+        })
+    })
+
+    it('transfers ownership', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 999999999999999999)
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert')
+            return tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0] })
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true')
+            return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]})
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event')
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the sender account')
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the receiver account')
+            assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount')
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250000, 'adds the amount to to the receiving account')
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance, 750000, 'deducts the amount from the sender account')
         })
     })
 })
